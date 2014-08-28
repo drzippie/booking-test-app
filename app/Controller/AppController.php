cb3a36535d373c6f131d82f1fbf6298882394f08
DrZippie <antonio@antoniocortes.com>
2014-08-28 13:14:26 +0200

    Adding DebugKit and initial Auth system

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -22,4 +22,68 @@ App::uses('Controller', 'Controller');
  * @link		http://book.cakephp.org/2.0/en/controllers.html#the-app-controller
  */
 class AppController extends Controller {
+
+
+    public $components = array(
+    	'DebugKit.Toolbar' => array('panels' => array('history' => false)), 
+        'Session',
+        'Cookie',
+        'RequestHandler' ,
+         'Auth' => array(
+        	   'authenticate' => array(
+            'Form' => array(
+                'fields' => array('username' => 'email')
+            )
+        	),
+            'loginRedirect' => array('controller' => 'users', 'action' => 'zonaInterna' , 'admin' => false ),
+            'logoutRedirect' => array('controller' => 'home', 'action' => 'index',  'admin' => false )
+        )
+    );
+
+
+    public function beforeFilter() {
+      	if ( $this->params['admin'] && $this->Auth->user( 'rol' ) != 'admin' && ( $this->request->params['action'] != 'login') ) {
+     	 
+     		$this->redirect( '/users/login') ;
+    	}
+ 		  $this->Auth->fields = array(
+            'username' => 'email',
+            'password' => 'password'
+        );
+        $this->Auth->allow('index', 'view');
+        $this->Auth->allow();
+        $this->set( 'user',  $this->Auth->user());
+         parent::beforeFilter();
+
+
+     }
+    public function isAuthorized($user = null) {
+         // Any registered user can access public functions
+         if (empty($this->request->params['admin'])) {
+            return true;
+        }
+
+        // Only admins can access admin functions
+        if (isset($this->request->params['admin'])) {
+            return (bool)($user['role'] === 'admin');
+        }
+
+        // Default deny
+        return false;
+    }
+ 	public function loginUser( $email, $password) {
+
+	$this->loadModel( 'User');
+
+	$usuario = $this->User->find('first', array(
+			'conditions' => array(
+				'User.email' => $email,
+				'User.password' => AuthComponent::password( $password )
+				)
+		));
+		return $this->Auth->login( $usuario['User'] ) ;
+
+	}
+
+
 }
